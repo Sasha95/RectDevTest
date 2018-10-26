4efa127623800df4b5e56092f7ec172690d795ad
Мангир Александр <sasha982025@gmail.com>
2018-10-26 17:52:41 +0300

    Updates

---
+++ precache-manifest.c697dbbdff999d0155e09cfc6bde0606.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0db5cd3ed5423b632b12c51c53b3cb3",
+    "url": "/RectDevTest/static/media/list.f0db5cd3.svg"
+  },
+  {
+    "revision": "76024bd5ee26068d1819",
+    "url": "/RectDevTest/static/css/main.1e1f59c3.chunk.css"
+  },
+  {
+    "revision": "29e415442ba90423cdf7f430a939f4e6",
+    "url": "/RectDevTest/static/media/plus-buttonB.29e41544.svg"
+  },
+  {
+    "revision": "0e3a0873272331ccbd80",
+    "url": "/RectDevTest/static/js/1.0e3a0873.chunk.js"
+  },
+  {
+    "revision": "67c726480ae27401c25d",
+    "url": "/RectDevTest/static/js/runtime~main.67c72648.js"
+  },
+  {
+    "revision": "2855eb43460c78f0a6112cea594ba5f5",
+    "url": "/RectDevTest/static/media/house-black-silhouette-without-door.2855eb43.svg"
+  },
+  {
+    "revision": "76024bd5ee26068d1819",
+    "url": "/RectDevTest/static/js/main.76024bd5.chunk.js"
+  },
+  {
+    "revision": "5debbfba154661ee8229604b738d9b4f",
+    "url": "/RectDevTest/static/media/house-black-silhouette-without-door1.5debbfba.svg"
+  },
+  {
+    "revision": "458e462f45c67b3cc96046605d254075",
+    "url": "/RectDevTest/static/media/list1.458e462f.svg"
+  },
+  {
+    "revision": "32743245c2481a0ec99bc8e8eb7acfde",
+    "url": "/RectDevTest/static/media/plus-button.32743245.svg"
+  },
+  {
+    "revision": "0e3a0873272331ccbd80",
+    "url": "/RectDevTest/static/css/1.734125cf.chunk.css"
+  },
+  {
+    "revision": "a3e0ecd8fd9cbf17252a54f3e6094a54",
+    "url": "/RectDevTest/index.html"
+  }
+];
